ad424072bcdff14ea8c342281a758d4f910c39cf
Michał <hortexix@gmail.com>
2020-07-29 18:26:36 +0200

    Cleaning up unnecessary files. Adding a basic project structure

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BeerList } from "../components/BeerList";
+import { Header } from "../components/Header";
+
+export const ProductList = () => {
+  return (
+    <div>
+      <Header />
+      <BeerList />
+    </div>
+  );
+};
